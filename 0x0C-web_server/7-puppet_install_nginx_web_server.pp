3491a068fc8899c7c6ad1e5c0926a72deb645294
MAZTRO <design.mallet@gmail.com>
2020-03-26 17:19:42 +0000

    Add install nginx with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,8 +3,5 @@
 
 exec { 'nginx':
   provider => shell,
-  command  => 'sudo apt update && sudo apt install nginx -y'
-  command  => "sed -i '18 a \ \n\t rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default"
-  command  => 'echo "Holberton School for the win!" | sudo tee /var/www/html/index.html'
-  command  => 'sudo service nginx restart'
+  command  => 'sudo apt update && sudo apt install nginx -y && sed -i "18 a \ \n\t rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default && echo "Holberton School for the win!" | sudo tee /var/www/html/index.html && sudo service nginx restart'
 }
