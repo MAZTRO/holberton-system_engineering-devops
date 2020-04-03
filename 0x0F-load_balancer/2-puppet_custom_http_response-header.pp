02acd6b78ec4527e9fd41a47fafcc4a37dcc3aa7
MAZTRO <design.mallet@gmail.com>
2020-04-03 21:40:25 +0000

    Fix instalation of HAproxy

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,5 +3,5 @@
 
 exec { 'HTTP Config':
   provider => shell,
-  command  => "apt-get update && apt-get install nginx -y && echo 'Holberton School' | sudo tee /var/www/html/index.html && sed -i '18 a \ \n\t rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default && sed -i '/http {/a \ \tadd_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf && service nginx restart",
+  command  => "apt-get update && apt-get install haproxy -y && echo 'Holberton School' | sudo tee /var/www/html/index.html && sed -i '18 a \ \n\t rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default && sed -i '/http {/a \ \tadd_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf && service nginx restart",
 }
