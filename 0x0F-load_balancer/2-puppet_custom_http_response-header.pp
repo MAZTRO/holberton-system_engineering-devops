7ac0ad45627a5b2931af96dec6f9a72ed01e41b2
MAZTRO <design.mallet@gmail.com>
2020-04-03 21:29:01 +0000

    Add task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,7 @@
+# Script to install a gem with Puppet
+# Instal HAproxy and an initial config
+
+exec { 'HTTP Config':
+  provider => shell,
+  command  => "apt-get update && apt-get install nginx -y && echo 'Holberton School' | sudo tee /var/www/html/index.html && sed -i '18 a \ \n\t rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default && sed -i '/http {/a \ \tadd_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf && service nginx restart",
+}
