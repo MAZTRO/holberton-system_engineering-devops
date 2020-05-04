2fb6c9d04453633ba8c9dc25495c15314ffd70c5
MAZTRO <design.mallet@gmail.com>
2020-05-04 21:44:22 +0000

    Add '.get'

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
     Using what you did in the task #0,
-    extend your Python script to export data in the JSON format.
+    extend your Python script to export data in the JSON format,
+    to all user.
 """
 from sys import argv
 import json
@@ -18,11 +19,11 @@ if __name__ == "__main__":
 
     for user in users.json():
         for todo in todos.json():
-            if user['id'] == todo.get('userId'):
+            if user.get('id') == todo.get('userId'):
                 todo_list.append({"username": user.get('username'),
                                  "task": todo.get('title'),
                                  "completed": todo.get('completed')})
-        data[user['id']] = todo_list
+        data[user.get('id')] = todo_list
         todo_list = []
     with open('todo_all_employees.json', mode='w') as json_file:
         json.dump(data, json_file)
