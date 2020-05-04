549d6fffad27c5927a1b0d6d9a9a83185d877070
MAZTRO <design.mallet@gmail.com>
2020-05-04 22:02:43 +0000

    Change data key

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,12 +13,11 @@ if __name__ == "__main__":
     todos = requests.get(uri + 'todos')
     data = {}
     for user in users.json():
-        todo_list = []
-        for todo in todos.json():
-            if user.get('id') == todo.get('userId'):
-                todo_list.append({"username": user.get('username'),
-                                 "task": todo.get('title'),
-                                 "completed": todo.get('completed')})
-        data[user.get('id')] = todo_list
+        data[user.get('id')] = []
+        for td in todos.json():
+            if user.get('id') == td.get('userId'):
+                data[user.get('id')].append({"username": user.get('username'),
+                                            "task": td.get('title'),
+                                            "completed": td.get('completed')})
     with open('todo_all_employees.json', mode='w') as json_file:
         json.dump(data, json_file)
