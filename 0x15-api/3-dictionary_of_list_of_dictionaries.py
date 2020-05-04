5f92b5633d76e6f6443e32e679add0f1ab416f37
MAZTRO <design.mallet@gmail.com>
2020-05-04 21:55:29 +0000

    Remove SYS

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,17 +11,14 @@ if __name__ == "__main__":
     uri = 'https://jsonplaceholder.typicode.com/'
     users = requests.get(uri + 'users')
     todos = requests.get(uri + 'todos')
-
-    todo_list = []
     data = {}
-
     for user in users.json():
+        todo_list = []
         for todo in todos.json():
             if user.get('id') == todo.get('userId'):
                 todo_list.append({"username": user.get('username'),
                                  "task": todo.get('title'),
                                  "completed": todo.get('completed')})
         data[user.get('id')] = todo_list
-        todo_list = []
     with open('todo_all_employees.json', mode='w') as json_file:
         json.dump(data, json_file)
