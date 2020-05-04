d041b90f404985c83991fa11dc489279de3e4e03
MAZTRO <design.mallet@gmail.com>
2020-05-04 22:30:11 +0000

    Change var names

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-""" Python script to export data in the JSON format """
+"""returns all information about his/her todo list progress in json"""
 import json
 import requests
 
 if __name__ == "__main__":
-    users = requests.get('https://jsonplaceholder.typicode.com/users/')
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = 'https://jsonplaceholder.typicode.com/users/'
+    todos = 'https://jsonplaceholder.typicode.com/todos'
+    d_user = requests.get(users)
+    d_todos = requests.get(todos)
+    data_user = d_user.json()
+    data_todo = d_todos.json()
     data = {}
-    for user in users.json():
-        data[user.get('id')] = []
-        for td in todos.json():
-            if user.get('id') == td.get('userId'):
-                data[user.get('id')].append({"username": user.get('username'),
-                                            "task": td.get('title'),
-                                            "completed": td.get('completed')})
-    con = "todo_all_employees" + ".json"
-    with open(con, mode='w') as json_file:
+    for user in data_user:
+        data[user['id']] = []
+        for todo in data_todo:
+            if todo['userId'] == user['id']:
+                data[user['id']].append({
+                    'task': todo['title'],
+                    'completed': todo['completed'],
+                    'username': user['username']
+                    })
+    concat = 'todo_all_employees' + '.json'
+    with open(concat, mode='w') as json_file:
         json.dump(data, json_file)
